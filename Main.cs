eba1e360570a78bc0b48ea0837da932a126fd5b5
tjwogud <rlatjsal78@gmail.com>
2023-01-16 22:57:24 +0900

    init

---
+++ Main.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace ChapterReversalMod
+{
+    public class Main : ModInitializer
+    {
+        public static string Id { get; } = "ChapterReversalMod";
+        public static Harmony Harmony { get; private set; }
+
+        public override void OnInitializeMod()
+        {
+            Harmony = new Harmony(Id);
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
